ea554fb4f740220ea41c7259d9e7bf3fbb45f2d4
Tom Verbeure <hombre+github.com@gmail.com>
2024-05-26 17:10:35 -0700

    Blink LED. Mark correct resistors as installed.

--- sw/rp2040_v1/gps_interposer.cpp
+++ sw/rp2040_v1/gps_interposer.cpp
@@ -4,6 +4,9 @@
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 
+// https://github.com/ForsakenNGS/Pico_WS2812
+#include "WS2812.hpp"
+
 #define UART0_ID            uart0
 #define UART0_TX_PIN        0
 #define UART0_RX_PIN        1
@@ -13,6 +16,10 @@
 #define UART1_TX_PIN        4
 #define UART1_RX_PIN        5
 #define UART1_BAUD_RATE     9600
+
+#define LED_PIN             16
+#define LED_LENGTH 1
+#define LED_BLINK_PERIOD_US 1000000
 
 enum eState {
     WAIT_FIRST_0X40     = 0,
@@ -36,6 +43,8 @@ void hw_init()
     gpio_set_function(UART1_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART1_RX_PIN, GPIO_FUNC_UART);
     uart_set_hw_flow(UART1_ID, false, false);
+
+    
 }
 
 int get_msg_len(char *msg_id, bool req)
@@ -76,7 +85,7 @@ int get_msg_len(char *msg_id, bool req)
     return -1;
 }
 
-void xmit_msg(void *uart_id, char msg[], int len)
+void xmit_msg(uart_inst_t *uart_id, char msg[], int len)
 {
     for(int i=0;i<len;++i){
         uart_putc(uart_id, msg[i]);
@@ -94,9 +103,28 @@ char calc_checksum(char msg[], int start, int end)
     return checksum;
 }
 
+void test_alive()
+{
+    while(1){
+        printf("Hello, world!\n");
+        uart_puts(UART0_ID, "Hello world UART0\n");
+        uart_puts(UART1_ID, "Hello world UART1\n");
+        sleep_ms(1000);
+    }
+}
+
 int main() {
     stdio_init_all();
     hw_init();
+
+    WS2812 ledStrip(LED_PIN, LED_LENGTH, pio0, 0, WS2812::FORMAT_RGB);
+
+    ledStrip.fill( WS2812::RGB(64, 0, 0) );
+    ledStrip.show();
+
+    #if 0
+    test_alive();
+    #endif
 
     enum eState state = WAIT_FIRST_0X40;
 
@@ -116,8 +144,33 @@ int main() {
 
     char checksum = 0;
 
+    uint64_t    next_led_flip_time_us   = time_us_64() + LED_BLINK_PERIOD_US;
+    int         led_state = 1;
+
     while(true){
-        char c = uart_getc(UART0_ID);
+
+        char c;
+        if (uart_is_readable(UART0_ID)){
+            c = uart_getc(UART0_ID);
+            next_led_flip_time_us  = time_us_64() + LED_BLINK_PERIOD_US;
+
+            if (led_state != 2){
+                ledStrip.fill( WS2812::RGB(0, 64, 0) );
+                ledStrip.show();
+            }
+            led_state   = 2;
+        }
+        else{
+            if (time_us_64() > next_led_flip_time_us){
+                printf("%d ", led_state);
+                next_led_flip_time_us   += LED_BLINK_PERIOD_US;
+
+                led_state   = led_state ? 0 : 1;
+                ledStrip.fill( WS2812::RGB(64 * led_state, 0, 0) );
+                ledStrip.show();
+            }
+            continue;
+        }
 
         tx_buf[offset++] = c;
 
@@ -245,11 +298,5 @@ int main() {
             }
         }
 
-        #if 0
-        printf("Hello, world!\n");
-        uart_puts(UART0_ID, "Hello world UART0\n");
-        uart_puts(UART1_ID, "Hello world UART1\n");
-        sleep_ms(1000);
-        #endif
     }
 }
